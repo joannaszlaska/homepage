f4da7d6228310dbc3add19d14fe56371bac2952a
Joanna Szlaska <joanna.szlaska8@gmail.com>
2022-05-27 20:55:28 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log("cześć")
+let main__buttonRemove = document.querySelector(".js-main__buttonRemove");
+let main__img = document.querySelector(".js-main__img");
+let main__button = document.querySelector(".js-main__button");
+let body = document.querySelector(".body");
+let main__span = document.querySelector(".main__span");
+
+main__buttonRemove.addEventListener("click", () => {
+    main__img.remove();
+
+});
+main__button.addEventListener("click", () => {
+    body.classList.toggle("body__dark");
+
+    main__span.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+
+});
+
+
