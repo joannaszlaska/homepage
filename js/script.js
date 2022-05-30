8bb67a917530c9f0b3c1da274f8b599f0c7630cb
Joanna Szlaska <joanna.szlaska8@gmail.com>
2022-05-30 22:56:18 +0200

    class changed

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ console.log("cześć")
 let main__buttonRemove = document.querySelector(".js-main__buttonRemove");
 let main__img = document.querySelector(".js-main__img");
 let main__button = document.querySelector(".js-main__button");
-let body = document.querySelector(".body");
-let main__span = document.querySelector(".main__span");
+let body__dark = document.querySelector(".body__dark");
+let main__span = document.querySelector(".js-main__span");
 
 main__buttonRemove.addEventListener("click", () => {
     main__img.remove();
@@ -12,7 +12,7 @@ main__buttonRemove.addEventListener("click", () => {
 main__button.addEventListener("click", () => {
     body.classList.toggle("body__dark");
 
-    main__span.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+    main__span.innerText = body.classList.contains("body__dark") ? "jasny" : "ciemny";
 
 });
 
